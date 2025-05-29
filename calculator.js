71ee40b42748806def6164505f034dc269811fe9
Jacob <stobnicki.jake@gmail.com>
2025-05-28 20:13:43 -0400

    Added the ability to enter a first number and then an operator to the display. Need to add the ability to begin entering the second number once an operator is selected.

--- calculator.js
+++ calculator.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log(add(4, 5));
-    console.log(subtract(4, 5));
-    console.log(multiply(4, 5));
-    console.log(divide(4, 5));
 
+    // Add event listener to number buttons
     let digitButtons = document.querySelectorAll('.btn-number');
     for (let button of digitButtons) {
+        let digitValue = button.dataset.buttonValue
         button.addEventListener('click', () => {
-        display(button.dataset.buttonValue)
+        display(digitValue)
         })
     }
 
+    // Add event listener to operator buttons
     let operatorButtons = document.querySelectorAll('.btn-operator');
     for (let button of operatorButtons) {
+        let operatorValue = button.dataset.buttonValue
         button.addEventListener('click', () => {
-        display(button.dataset.buttonValue)
+        display(operatorValue)
         })
     }
     
@@ -51,25 +51,32 @@ function divide(x, y) {
     return x / y
 }
 
-function display(button) {
-    console.log(button)
+function display(buttonValue) {
     // Store the display div and button pressed
-    const display = document.querySelector('#calculatorDisplayBottom');
-    let currentDisplay = document.createElement('span')
-    let displayValue
+    let outputDisplay = document.querySelector('#calculatorFirstNumText');
+    let displayContainer = document.querySelector('#calculatorDisplayBottom')
+    let displayValue = document.querySelector('#calculatorFirstNumText').innerHTML;
 
-    // If the number is a digit (0-9)
-    if (0 >= button <= 9) {
-        currentDisplay.innerHTML = `${button}`
-        if (displayValue === undefined) {
-            displayValue = button
+    // If the buttonValue is a digit (0-9) and no operator exists
+    if (buttonValue <= 9 && buttonValue >= 0) {
+        if (displayValue === 0) {
+            displayValue = buttonValue
         } else {
-            let newDisplayValue = displayValue.toString() + button
+            let newDisplayValue = displayValue.toString() + buttonValue
             displayValue = parseInt(newDisplayValue)
         }
         console.log(displayValue)
-        display.appendChild(currentDisplay)
-    }  
+        outputDisplay.innerHTML = `${displayValue}`
+    }
+
+    // If the buttonValue is an operator
+    else if (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/') {
+        let operatorValue = document.createElement('span')
+        operatorValue.innerHTML = `${buttonValue}`
+        displayContainer.appendChild(operatorValue)
+    }
+    
+    // If buttonValue is a digit (0-9) and an operator exists
 
 
 }
